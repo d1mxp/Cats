e2660c8e469b09ef77db903bb6146a35fbb660ee
D <dimmxp@gmail.com>
2023-03-23 00:14:51 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,52 +1,205 @@
-const cardsContainer = document.querySelector(".cards");
-const btnOpenPopup = document.querySelector(".btn");
-const formCatAdd = document.querySelector("#popup-form-add");
+import { api } from './api.js';
+import { Card } from './card.js';
+import { CatsInfo } from './cats-info.js';
+import { PopupWithImage } from './popup-image.js';
+import { Popup } from './popup.js';
+import './utils.js';
 
-const popupAdd = new Popup("popup-add");
-const popupImage = new PopupWithImage("popup-cat-image");
+const cardsContainer = document.querySelector('.cards');
+const btnOpenPopup = document.querySelector('.btn_add');
+const btnOpenPopupLogin = document.querySelector(".login");
+
+const formCatAdd = document.querySelector('#popup-form-add');
+const formLogin = document.querySelector("#popup-form-login");
+const isAuth = Cookies.get("email");
+const MAX_LIVE_STORAGE = 10;
+
+const popupAdd = new Popup('popup-add');
+const popupImage = new PopupWithImage('popup-cat-image');
+const popupLogin = new Popup("popup-login");
+
+const popupCatInfo = new Popup("popup-cat-info");
+const catsInfoInstance = new CatsInfo(
+    "#cats-info-template",
+    handleEditCatInfo,
+    handleLike,
+    handleCatDelete
+    );
+
+const catsInfoElement = catsInfoInstance.getElement();
 
 function serializeForm(elements) {
-  const formData = {};
+    const formData = {};
 
-  elements.forEach((input) => {
-    if (input.type === "submit" || input.type === "button") return;
-    if (input.type === "checkbox") {
-      formData[input.name] = input.checked;
-    }
-    if (input.type !== "checkbox") {
-      formData[input.name] = input.value;
-    }
-  });
+    elements.forEach(input => {
+        if (input.type === 'submit' || input.type === 'button') return
+        if (input.type === 'checkbox') {
+            formData[input.name] = input.checked;
+        }
+        if (input.type !== 'checkbox') {
+            formData[input.name] = input.value;
+        }
+    })
 
-  return formData;
+    return formData;
+}
+
+function createCat(dataCat){
+    const newElement = new Card(
+        dataCat, 
+        "#card-template", 
+        handleClickCatImage, 
+        handleCatTitle,
+        handleLike
+    );
+    cardsContainer.prepend(newElement.getElement());
 }
 
 function handleFormAddCat(e) {
-  e.preventDefault();
-  const elementsFormCat = [...formCatAdd.elements];
-  const formData = serializeForm(elementsFormCat);
-  console.log(formData);
-  const newElement = new Card(formData, "#card-template", handleClickCatImage);
-  cardsContainer.prepend(newElement.getElement());
-
-  popupAdd.close();
+    e.preventDefault();
+    const elementsFormCat = [...formCatAdd.elements];
+    const formData = serializeForm(elementsFormCat);
+    api.addNewCat(formData)
+    .then(function() {
+        createCat(formData);
+        updateLocalStorage(formData, {type: 'ADD_CAT'});
+        popupAdd.close();
+    })
+    .catch(function(err){
+        console.log(err);
+    })
 }
 
 function handleClickCatImage(dataSrc) {
-  popupImage.open(dataSrc);
+    popupImage.open(dataSrc)
 }
 
-formCatAdd.addEventListener("submit", handleFormAddCat);
+function handleFormLogin(e){
+    e.preventDefault();
+    const elementsFormLogin = [...formLogin.elements];
+    const formData = serializeForm(elementsFormLogin);
+    Cookies.set("email", formData.email, {expires: 7});
+    btnOpenPopup.classList.remove('visually-hidden');
+    btnOpenPopupLogin.classList.add('visually-hidden');
+    popupLogin.close()
+}
 
-btnOpenPopup.addEventListener("click", (e) => {
-  e.preventDefault();
-  popupAdd.open();
+btnOpenPopup.addEventListener('click', (e) => {
+    e.preventDefault();
+    popupAdd.open()
+})
+
+btnOpenPopupLogin.addEventListener("click", (e) => {
+    e.preventDefault();
+    popupLogin.open();
 });
 
-cats.forEach((catData) => {
-  const newElement = new Card(catData, "#card-template", handleClickCatImage);
-  cardsContainer.append(newElement.getElement());
-});
+function setDataRefresh(minute, key){
+    const setTime = new Date(new Date().getTime() + minute*60000)
+    localStorage.setItem(key, setTime);
+    return setTime;
+}
+
+function updateLocalStorage(data, action) { // {type: 'ADD_CAT'} {type: 'ALL_CATS'}
+    const oldStorage = JSON.parse(localStorage.getItem('cats'));
+  
+    switch (action.type) {
+        case 'ADD_CAT':
+            oldStorage.push(data);
+            localStorage.setItem('cats', JSON.stringify(oldStorage));
+            return;
+        case 'ALL_CATS':
+            setDataRefresh(MAX_LIVE_STORAGE, 'catsRefresh');
+            localStorage.setItem('cats',  JSON.stringify(data));
+            return;
+        case 'DELETE_CAT':
+            const newStorage = oldStorage.filter(cat => cat.id !== data.id);
+            localStorage.setItem('cats',  JSON.stringify(newStorage));
+            return;
+        case 'EDIT_CAT':
+            const updateStorage = oldStorage.map(cat => cat.id === data.id ? data : cat);
+            localStorage.setItem('cats',  JSON.stringify(updateStorage));
+            return;
+    }
+}
+
+function checkLocalStorage() {
+    const localData = JSON.parse(localStorage.getItem('cats')); //null
+    const getTimeExpires = localStorage.getItem('catsRefresh');
+
+    if(localData && localData.length && new Date() < new Date(getTimeExpires)){
+    localData.forEach((catData) => {
+        createCat(catData);
+    });
+    } else {
+    api.getAllCats()
+        .then(dataCats => {
+            dataCats.forEach((catData) => {
+            createCat(catData);
+            });
+            updateLocalStorage(dataCats, {type: 'ALL_CATS'});
+        })
+        .catch(function(err){
+        console.log(err);
+        })
+    }
+}
+
+function handleCatTitle(cardInstance) {
+
+    catsInfoInstance.setData(cardInstance);
+    popupCatInfo.setContent(catsInfoElement);
+    popupCatInfo.open();
+}
+
+function handleLike(data, cardInstance) {
+    const {id, favorite} = data;
+    api.updateCatById(id, {favorite})
+        .then(() => {
+            if(cardInstance) {
+                cardInstance.setData(data);
+                cardInstance.updateView();
+            }
+
+            updateLocalStorage(data, {type: 'EDIT_CAT'});
+        })
+}
+
+function handleCatDelete(cardInstance) {
+    api.deleteCatById(cardInstance.getId())
+        .then(() => {
+            cardInstance.deleteViev();
+            updateLocalStorage(cardInstance.getData(), {type: 'DELETE_CAT'});
+            popupCatInfo.close();
+        })
+}
+
+function handleEditCatInfo(cardInstance, data) {
+    const {age, description, name, id} = data;
+
+    api.updateCatById(id, {age, description, name})
+        .then(() => {
+            cardInstance.setData(data);
+            cardInstance.updateView();
+
+            updateLocalStorage(data, {type: 'EDIT_CAT'})
+            popupCatInfo.close();
+        })
+}
+
+formCatAdd.addEventListener('submit', handleFormAddCat)
+formLogin.addEventListener("submit", handleFormLogin);
+
+if(!isAuth) {
+    popupLogin.open();
+    btnOpenPopup.classList.add('visually-hidden');
+} else {
+    btnOpenPopupLogin.classList.add('visually-hidden');
+}
 
 popupAdd.setEventListener();
 popupImage.setEventListener();
+popupLogin.setEventListener();
+popupCatInfo.setEventListener();
+
+checkLocalStorage()
--- src/popup.js
+++ src/popup.js
@@ -1,31 +1,37 @@
-class Popup {
-    #handleEscUp = (evt) => {
-      if (evt.key === "Escape") {
+export class Popup {
+  #handleEscUp = (evt) => {
+    if (evt.key === "Escape") {
+      this.close();
+    }
+  };
+  constructor(classPopup) {
+    this._popupElement = document.querySelector(`.${classPopup}`);
+  }
+
+  setContent(contentNode) {
+    const containerContent = this._popupElement.querySelector('.popup__content');
+    containerContent.innerHTML = "";
+    containerContent.append(contentNode);
+  }
+
+  open() {
+    this._popupElement.classList.add(`active`);
+    document.addEventListener("keyup", this.#handleEscUp);
+  }
+
+  close() {
+    this._popupElement.classList.remove(`active`);
+    document.removeEventListener("keyup", this.#handleEscUp);
+  }
+
+  setEventListener() {
+    this._popupElement.addEventListener("mousedown", (evt) => {
+      if (
+        evt.target.classList.contains("popup") ||
+        !!evt.target.closest(".popup__close")
+      ) {
         this.close();
       }
-    };
-    constructor(classPopup) {
-      this._popupElement = document.querySelector(`.${classPopup}`);
-    }
-  
-    open() {
-      this._popupElement.classList.add(`active`);
-      document.addEventListener("keyup", this.#handleEscUp);
-    }
-  
-    close() {
-      this._popupElement.classList.remove(`active`);
-      document.removeEventListener("keyup", this.#handleEscUp);
-    }
-  
-    setEventListener() {
-      this._popupElement.addEventListener("mousedown", (evt) => {
-        if (
-          evt.target.classList.contains("popup") ||
-          !!evt.target.closest(".popup__close")
-        ) {
-          this.close();
-        }
-      });
-    }
+    });
   }
+}
